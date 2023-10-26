4bc0cce09a89506c6cd667d573a7d48102c7daac
vladn79 <vladdd.nov@gmail.com>
2023-10-26 23:41:53 +0300

    added overloaded basic operators +/-

--- LongInt.h
+++ LongInt.h
@@ -1,23 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 
 class LongInt {
 private:
     std::vector<int> digits;
 
 public:
-    LongInt(std::string n_str) {
-        for (char digit : n_str) {
+    LongInt() {}
+
+    LongInt(const std::string& numStr) {
+        for (char digit : numStr) {
             digits.push_back(digit - '0');
         }
+        std::reverse(digits.begin(), digits.end());
     }
 
-    void printNumber() const {
-        for (int digit : digits) {
-            std::cout << digit;
+    LongInt operator+(const LongInt& other) const {
+        LongInt result;
+        int carry = 0;
+        int maxSize = std::max(digits.size(), other.digits.size());
+
+        for (int i = 0; i < maxSize; ++i) {
+            int sum = carry;
+            if (i < digits.size()) {
+                sum += digits[i];
+            }
+            if (i < other.digits.size()) {
+                sum += other.digits[i];
+            }
+            carry = sum / 10;
+            result.digits.push_back(sum % 10);
         }
-        std::cout << std::endl;
+
+        if (carry > 0) {
+            result.digits.push_back(carry);
+        }
+
+        return result;
+    }
+
+
+    LongInt operator-(const LongInt& other) const {
+        LongInt result;
+        int borrow = 0;
+        int maxSize = std::max(digits.size(), other.digits.size());
+
+        for (int i = 0; i < maxSize; ++i) {
+            int diff = digits[i] - borrow;
+            if (i < other.digits.size()) {
+                diff -= other.digits[i];
+            }
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+            borrow = 0;
+            }
+            result.digits.push_back(diff);
+    }
+
+        while (result.digits.size() > 1 && result.digits.back() == 0) {
+            result.digits.pop_back();
+        }
+
+    return result;
+}
+
+    friend std::ostream& operator<<(std::ostream& os, const LongInt& num) {
+        for (auto it = num.digits.rbegin(); it != num.digits.rend(); it++) {
+            os << *it;
+        }
+        return os;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "LongInt.h"
-
-
 int main() {
+    std::string num1 = "1111111111111111111111111111111111111111";
+    std::string num2 = "3333333333333333333333333333333333333333";
 
-    std::string numberStr = "12122341234123412341234123412341234123412343123121263712831293719283178230971902308912380128334567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+    LongInt longNum1(num1);
+    LongInt longNum2(num2);
 
-    LongInt longNum(numberStr);
+    LongInt sum = longNum1 + longNum2;
+    LongInt diff = longNum2 - longNum1;
 
-    longNum.printNumber();
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Diff: " << diff << std::endl;
+
 
     return 0;
 }
