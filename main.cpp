072c22c0255df43f79f0ac189a9ea67a2a81e6a2
vladn79 <vladdd.nov@gmail.com>
2023-11-14 14:01:17 +0200

    execution of *

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include "LongInt.h"
+//#include "Karatsuba.h"
 int main() {
-    std::string num1 = "1111111111111111111111111111111111111111";
-    std::string num2 = "3333333333333333333333333333333333333333";
+      std::string num1 = "11";
+      std::string num2 = "3";
 
-    LongInt longNum1(num1);
-    LongInt longNum2(num2);
+      LongInt longNum1(num1);
+      LongInt longNum2(num2);
 
-    LongInt sum = longNum1 + longNum2;
-    LongInt diff = longNum2 - longNum1;
+      LongInt sum = longNum1 + longNum2;
+      LongInt diff = longNum1 - longNum2;
+      LongInt pr = longNum1 * longNum2;
 
-    
 
-    std::cout << "Sum: " << sum << std::endl;
-    std::cout << "Diff: " << diff << std::endl;
+      std::cout << "Sum: " << sum << std::endl;
+      std::cout << "Diff: " << diff << std::endl;
+      std::cout << "prod: " << pr << std::endl;
 
 
     return 0;
