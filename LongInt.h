cd11bac057057a5b862336255325f93ac4197794
vladn79 <vladdd.nov@gmail.com>
2023-11-16 17:26:06 +0200

    added implementation of *

--- LongInt.h
+++ LongInt.h
@@ -19,6 +19,8 @@ public:
 
     LongInt operator+(const LongInt& other);
     LongInt operator-(const LongInt& other) const;
+    LongInt operator*(const LongInt& other) const;
+     
     friend std::ostream& operator<<(std::ostream& out, LongInt num);
     friend std::istream& operator>>(std::istream& in, LongInt& num);
 };
