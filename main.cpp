782ac4698643d8aa15e5b88a122a2e4714c6deaf
vladn79 <vladdd.nov@gmail.com>
2023-12-01 01:55:04 +0200

    SchönhageStrassen mult upd

--- main.cpp
+++ main.cpp
@@ -4,14 +4,32 @@
 #include <vector>
 using namespace std;
 
+
+std::vector<int> stringToVector(const LongInt& inputNum) {
+    std::vector<int> result;
+    for (auto it = inputNum.rbegin(); it != inputNum.rend(); ++it) {
+        result.push_back(*it - '0'); 
+    }
+    
+    return result;
+}
 int main() {
     LongInt a, b, c;
-    int n = 5;
+    cin >> a;
+    cin >> b;
 
-    cin >> c;
+    cout << "karatsuba = " << LongInt::karatsuba_multiply(a, b) << endl;
+    cout << "toom_cook = " << LongInt::toom_cook_multiply(a, b) << endl;
+    cout << "modular m = " << LongInt::shenhageMultiply(a, b) << endl;// таке собі...
+    
+    std::vector<int> coeffsA = stringToVector(a);
+    std::vector<int> coeffsB = stringToVector(b); 
 
+    LongInt numA(coeffsA);
+    LongInt numB(coeffsB);
 
-    cout << isPrimeFermat(c, n);
-    return 0;
-
+    LongInt result = LongInt::schonhageStrassenMultiply(numA, numB);
+    for (int i = result.coefficients.size() - 1; i >= 0; --i) {
+        std::cout << result.coefficients[i];
+    }
 }
